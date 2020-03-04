a47568dc0dfc76f2a109951cc65a6eb29004d017
Jeffrey Varner <jdv27@cornell.edu>
2020-03-03 20:46:18 -0500

    crowding constraint hack

--- src/flux/Solve.jl
+++ src/flux/Solve.jl
@@ -205,7 +205,7 @@ function maximize_specific_growth_rate(path_to_measurements_file::String, organi
         # re-run w/-1's in the crowding cosntaint -
         idx_negative = findall(x->x<0.0,calculated_flux_array_tmp)
         add_constraint = updated_data_dictionary["additional_constraint_array"]
-        add_constraint[end,idx_negative] .= -1.0
+        add_constraint[end,idx_negative] .= -10.0
         updated_data_dictionary["additional_constraint_array"] = add_constraint
         (objective_value, calculated_flux_array, dual_value_array, uptake_array, exit_flag, status_flag) = calculate_optimal_flux_distribution(updated_data_dictionary);
 
