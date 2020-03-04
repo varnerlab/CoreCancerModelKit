585d2d40fbd8de92d97283cc4d215acf4be8984a
Jeffrey Varner <jdv27@cornell.edu>
2020-03-04 10:03:11 -0500

    working with convex

--- src/flux/Types.jl
+++ src/flux/Types.jl
@@ -33,6 +33,7 @@ mutable struct VLOptimalConvexFluxResult
     flux_array::Array{Float64,1}
     status_flag::Symbol
     flux_bounds_array::Array{Float64,2}
+    estimated_metabolite_array::Array{Float64,1}
 
     # constructor -
     function VLOptimalConvexFluxResult()
